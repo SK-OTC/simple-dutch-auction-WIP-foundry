c377d2557c744ad87906185571e86cd58c131ca9
Joy <168293419+SK-OTC@users.noreply.github.com>
2026-01-15 15:08:37 -0500

    Added logic

--- src/DutchAuction.sol
+++ src/DutchAuction.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 contract DutchAuction {
     //   An auction where the price starts high and decreases over time.
+    event PurchaseItem(address indexed buyer, uint256 amountPaid, uint256 itemId);
     address public seller;
     address public buyer;
     uint256 public itemId;
@@ -13,6 +14,65 @@ contract DutchAuction {
     uint256 public auctionEndTime;
     uint256 public decrementInterval;
     bool public isSold;
+
+    modifier onlyBeforeEnd() {
+        require(block.timestamp <= auctionEndTime, "Auction has ended");
+        _;
+    }
+
+    modifier onlyAfterStart() {
+        require(block.timestamp >= auctionStartTime, "Auction has not started yet");
+        _;
+    }
+
+    modifier onlyNotSold() {
+        require(isSold == false, "Item has already been sold");
+        _;
+    }
+
+    constructor(uint256 _itemId, uint256 _startPrice, uint256 _reservePrice, uint256 _priceDecrement, uint256 _decrementInterval, uint256 _auctionDuration) {
+        seller = msg.sender;
+        itemId = _itemId;
+        startPrice = _startPrice;
+        reservePrice = _reservePrice;
+        priceDecrement = _priceDecrement;
+        decrementInterval = _decrementInterval;
+        auctionStartTime = block.timestamp;
+        auctionEndTime = auctionStartTime + _auctionDuration;
+        isSold = false;
+    }
+
+
+    function getPrice() public view onlyBeforeEnd onlyAfterStart onlyNotSold returns (int256) {
+        // Calculate current price based on time elapsed using linear decay formula: 
+        // startingPrice - (discountRate * timeElapsed)
+        require(itemId != 0, "Item does not exist"); 
+
+        uint256 timeElapsed = block.timestamp - auctionStartTime;
+        uint256 discountRate = priceDecrement / decrementInterval;
+        int256 currentPrice = int256(startPrice) - int256(discountRate * timeElapsed);
+        if (discountRate * timeElapsed >= startPrice) {
+                return int256(reservePrice); 
+        }
+        if (currentPrice < int256(reservePrice)) {
+            currentPrice = int256(reservePrice);
+        }        
+        return currentPrice;
+    }
+
+    function buy() public payable onlyBeforeEnd onlyAfterStart onlyNotSold {
+        require(isSold == false, "This auction item has been sold");
+        require(itemId != 0, "Item does not exist");
+        int256 currentPrice = getPrice();
+        require(msg.value >= uint256(currentPrice), "Insufficient funds to buy the item");
+        buyer = msg.sender;
+        // Reentrancy guard
+        isSold = true;
+        (bool success,) = buyer.call{value: msg.value}(new bytes(0));
+        require(success, "tx failed");
+       
+        emit PurchaseItem(buyer, msg.value, itemId);
+    }
 
 }
 
--- test/DutchAuction.t.sol
+++ test/DutchAuction.t.sol
@@ -1,14 +1,45 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.30;
 
-contract Counter {
-    uint256 public number;
+import {Test, console2, StdStyle} from "forge-std/Test.sol";
+import {DutchAuction} from "../src/DutchAuction.sol";
+contract DutchAuctionTest is Test {
+    DutchAuction public auction;
+    address public seller;
+    address public buyer1;
+    address public buyer2;
+    uint256 public itemId = 1;
+    uint256 public startPrice = 10 ether;
+    uint256 public reservePrice = 2 ether;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+
+    function setUp() public {
+        seller = payable(address(0x1));
+        buyer1 = payable(address(0x2));
+        buyer2 = payable(address(0x3));
+        vm.prank(seller);
+        auction = new DutchAuction(itemId, startPrice, reservePrice, 1 ether, 1 minutes, 10 minutes);
+        vm.deal(address(auction), 5 ether);
+    }
+    
+    // 1. Test Auction Initialization
+    function testAuctionInitialization() public {
+        assertEq(auction.seller(), seller);
+        assertEq(auction.itemId(), itemId);
+        assertEq(auction.startPrice(), startPrice);
+        assertEq(auction.reservePrice(), reservePrice);
+    }   
+
+    function testGetPriceAtStart() public {
+        
+
     }
 
-    function increment() public {
-        number++;
-    }
+      // 5. Test Edge Cases
+        // Multiple buyers attempting simultaneously
+        // Price reaching reserve
+    
+
 }
+
+  
